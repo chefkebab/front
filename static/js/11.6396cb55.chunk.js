c6b1fa5a7771004f91b0b5cd20b04d06ed49ef13
Kebab Chef <kebabchef@gmail.com>
2021-01-28 16:03:55 +0100

    update kebab

---
+++ static/js/11.6396cb55.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[11],{788:function(n,p){}}]);
+//# sourceMappingURL=11.6396cb55.chunk.js.map
--- static/js/11.98ff5623.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[11],{817:function(n,p){}}]);
-//# sourceMappingURL=11.98ff5623.chunk.js.map
